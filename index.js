b19c6c7b398635282b02b774018d784998709ac3
Anton Sorokey <ansorokey@gmail.com>
2024-10-31 11:45:59 -0400

    query selectors

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const inpProb = document.querySelector("#inpProb");
+const inpSize = document.querySelector("#inpSize");
+const btnCalc = document.querySelector("#btnCalc");
 const results = document.querySelector("#results");
 
 results.value = "Hello world";
