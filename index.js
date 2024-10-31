37f9b32f09860003b630e2b846f7bc86fb071052
Anton Sorokey <ansorokey@gmail.com>
2024-10-31 11:51:40 -0400

    calculation

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ const inpSize = document.querySelector("#inpSize");
 const btnCalc = document.querySelector("#btnCalc");
 const results = document.querySelector("#results");
 
+function calcStdDev(p, n, round=2) {
+    return Math.sqrt(
+        (p*(1-p)) / n
+    )
+}
+
+btnCalc.addEventListener("click", () => {
+    let res = calcStdDev(npProb.value, inpSize.value);
+    results.value = res;
+})
+    
+
 results.value = "Hello world";
