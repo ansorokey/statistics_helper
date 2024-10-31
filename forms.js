3adde11a4a2d5b650c2dfd05b2ff391555163989
Anton Sorokey <ansorokey@gmail.com>
2024-10-31 12:46:46 -0400

    std dev form

---
+++ forms.js
@@ -0,0 +1,28 @@
+const stdDev = ```
+    <h2>Standard Deviation</h2>
+
+    <label>
+        Probability:
+        <input 
+            id="a"
+            type="number" 
+            placeholder="0.0"                
+        >
+    </label>
+
+    <label>
+        Sample Size:
+        <input id="b" type="number" placeholder="000">
+    </label>
+
+    <button
+        id="btnCalc"
+    >
+        Calculate
+    </button>
+
+    <label>
+        Result:
+        <input id="results" readonly>
+    </label>
+```
